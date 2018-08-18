9f9680680e9cb8503c76ba2194ca1e31e432adc3
human <codingEzio@users.noreply.github.com>
2018-08-18 14:09:43 +0800

    update - text - [fix_typo]

--- shell/find-lt-1M-file.sh
+++ shell/find-lt-1M-file.sh
@@ -4,5 +4,5 @@
 #   l   detailed info
 #   S   sort file by size 
 #   h   human-readable (K,M,G etc.)
-#   M   not including (M) (then it must less than 1M)
+#   M   not including (M) (then it [may] less than 1M)
 ls -lSh * | grep -v M
