8db5439dce463663bc43f27ec4eff98784b67506
Ezio <assassinste@gmail.com>
2018-08-18 10:39:43 +0800

    add - file - [find-specific-size]

---
+++ shell/find-lt-1M-file.sh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+
+# [detail]
+#   l   detailed info
+#   S   sort file by size 
+#   h   human-readable (K,M,G etc.)
+#   M   not including (M) (then it must less than 1M)
+ls -lSh * | grep -v M
